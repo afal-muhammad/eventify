df4b7c501e8202642a00118a73ae5e64702e702d
afalmuhammed9@gmail.com <afalmuhammed9@gmail.com>
2021-12-30 17:53:19 +0530

    heroku deployment configs

--- evently/settings.py
+++ evently/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,8 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
 
 try:
-   from .local import *
+    from .local import *
 except:
-    pass
+    from .main_settings import *
+
