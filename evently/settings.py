621c58e28aac165cd429ca992b790b91f8765d99
afalmuhammed9@gmail.com <afalmuhammed9@gmail.com>
2022-01-02 00:20:53 +0530

    changes in settings\

--- evently/settings.py
+++ evently/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import dj_database_url
+
 import os
 import django_heroku
 from pathlib import Path
@@ -74,7 +76,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'evently.wsgi.application'
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 
