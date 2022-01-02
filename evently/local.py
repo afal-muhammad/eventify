0ed83ff250e1a35621d050e1211e9d41f977d492
afalmuhammed9@gmail.com <afalmuhammed9@gmail.com>
2022-01-02 13:59:06 +0530

    deleted local from git

--- evently/local.py
+++ evently/local.py
@@ -1,23 +1,10 @@
 import os
-from pathlib import Path
+from .settings import BASE_DIR
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'database.sqlite'),
     }
 }
