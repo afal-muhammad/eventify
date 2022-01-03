74066493f4b7ac0a41ce5cb267473d27795a4453
afalmuhammed9@gmail.com <afalmuhammed9@gmail.com>
2022-01-03 18:09:58 +0530

    production

--- evently/settings.py
+++ evently/settings.py
@@ -76,10 +76,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'evently.wsgi.application'
 
-
 DATABASES = {
-    'default': dj_database_url.config()
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'events',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'ramshi@123',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
+    }
+
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 
