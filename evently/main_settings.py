960c907fc8ef59a4ab5bd4cd02a9e33b809c1173
afalmuhammed9@gmail.com <afalmuhammed9@gmail.com>
2022-01-03 17:19:19 +0530

    production setting

--- evently/main_settings.py
+++ evently/main_settings.py
@@ -1,1 +1,19 @@
+DATABASES = {
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'events',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'ramshi@123',
+
+        'HOST': 'https://eventi-fy.herokuapp.com/',
+
+        'PORT': '5432',
+
+    }
+
+}
