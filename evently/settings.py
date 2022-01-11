0e093d62f9a407ab1ae466ab2655a6feed0596ac
afalmuhammed9@gmail.com <afalmuhammed9@gmail.com>
2022-01-11 17:37:42 +0530

    static

--- evently/settings.py
+++ evently/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+agkry!5r(emmtzvi5t7u=@gxk_rnnf+!cpo!p%u2quz3(-@q*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://eventi-fy.herokuapp.com']
 
 AUTH_USER_MODEL = 'accounts.User'
 
