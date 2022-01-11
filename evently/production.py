62a01319daae4f5f241deb27d1db7eef8af6915a
afalmuhammed9@gmail.com <afalmuhammed9@gmail.com>
2022-01-11 17:20:38 +0530

    prod

--- evently/production.py
+++ evently/production.py
@@ -1,3 +1,15 @@
+import os
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DEBUG = False
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
