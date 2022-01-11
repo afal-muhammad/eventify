f96ecde7d1853be175c34a8baf49f6ec0e37f25c
afalmuhammed9@gmail.com <afalmuhammed9@gmail.com>
2022-01-11 15:52:10 +0530

    404 and 500 handlers

--- evently/production.py
+++ evently/production.py
@@ -3,7 +3,5 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = False
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
