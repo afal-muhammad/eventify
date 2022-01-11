cbc4b5742bf1e193778372e7e1a9f12f52e6dc94
afalmuhammed9@gmail.com <afalmuhammed9@gmail.com>
2022-01-11 16:14:12 +0530

    production

--- evently/production.py
+++ evently/production.py
@@ -1,7 +1,1 @@
-import os
-from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 DEBUG = False
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
